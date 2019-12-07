ca936267a81022d9fa66b6233551a227c3cac1af
Danny Shen <shenfive@gmail.com>
2019-12-07 11:35:11 +0800

    Initial Commit

---
+++ HelloNetwork02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloNetwork02
+//
+//  Created by 申潤五 on 2019/12/7.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

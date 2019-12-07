d3e7dedfb224fa9abdd2089a3db3405003c19c2a
Danny Shen <shenfive@gmail.com>
2019-12-07 15:41:37 +0800

    King

--- HelloNetwork02/ViewController.swift
+++ HelloNetwork02/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
@@ -35,22 +36,9 @@ class ViewController: UIViewController {
         
         let urlString = "https://s.yimg.com/ny/api/res/1.2/UBe_uhOfJyDTjWqUBsKh.w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyNDI7aD03NzQuMjEyNTk4NDI1MTk2OA--/https://s.yimg.com/uu/api/res/1.2/6hyNAqNpIHFLe.L..WL49Q--~B/aD05NTA7dz0xNTI0O3NtPTE7YXBwaWQ9eXRhY2h5b24-/http://media.zenfs.com/zh-Hant-TW/homerun/nownews.com/b0804bfa09809e880f1acf1fe3c285ba"
         DispatchQueue.global().async {
-            if let url = URL(string: urlString){
-                do{
-                    let data = try Data(contentsOf: url)
-                    DispatchQueue.main.async {
-                        self.myImageView.image = UIImage(data: data)
-                        print(Date().timeIntervalSince1970 - self.start)
-                    }
-                }catch{
-                    print(error.localizedDescription)
-                }
-            }
+            self.myImageView.kf.setImage(with: URL(string: urlString))
+            print(Date().timeIntervalSince1970 - self.start)
         }
-        
-
-        
-        
     }
 
 

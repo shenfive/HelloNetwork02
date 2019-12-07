7f50928af985e03944f2f322efca643d480e51a6
Danny Shen <shenfive@gmail.com>
2019-12-07 14:06:34 +0800

    bound

--- HelloNetwork02/ViewController.swift
+++ HelloNetwork02/ViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var containerView: UIView!
     let start = Date().timeIntervalSince1970
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        containerView.clipsToBounds = false
+        containerView.backgroundColor = UIColor.clear
+        containerView.layer.shadowRadius = 20         //陰影
+        containerView.layer.shadowOpacity = 0.6;
+        containerView.layer.shadowColor = UIColor.gray.cgColor
+        containerView.layer.shadowOffset = CGSize(width: 10, height: 10)
+        
         myImageView.clipsToBounds = true
-        myImageView.layer.cornerRadius = 15
-        myImageView.layer.borderColor = UIColor.red.cgColor
-        myImageView.layer.borderWidth = 2
+        myImageView.layer.cornerRadius = myImageView.frame.width / 2         //圓角
+        myImageView.layer.borderColor = UIColor.red.cgColor //邊線色
+        myImageView.layer.borderWidth = 5          //邊線
+
+        
+        
+        
         
         let urlString = "https://s.yimg.com/ny/api/res/1.2/UBe_uhOfJyDTjWqUBsKh.w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyNDI7aD03NzQuMjEyNTk4NDI1MTk2OA--/https://s.yimg.com/uu/api/res/1.2/6hyNAqNpIHFLe.L..WL49Q--~B/aD05NTA7dz0xNTI0O3NtPTE7YXBwaWQ9eXRhY2h5b24-/http://media.zenfs.com/zh-Hant-TW/homerun/nownews.com/b0804bfa09809e880f1acf1fe3c285ba"
         DispatchQueue.global().async {
